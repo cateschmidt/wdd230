0963c00c6a9228151f03d023122a0432b9ccbaca
cateschmidt <c8schmidt@gmail.com>
2023-03-15 09:13:03 -0600

    fix windchill and css

--- lesson10/chamber/scripts/weatherapi.js
+++ lesson10/chamber/scripts/weatherapi.js
@@ -3,7 +3,9 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=2e52b28e193c5dbda364e2b9891de906';
+
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=idaho&units=imperial&appid=2e52b28e193c5dbda364e2b9891de906';
 
 async function apiFetch() {
     try {
@@ -21,7 +23,7 @@ async function apiFetch() {
   }
   
   apiFetch();
-
+  
   function displayResults(weatherData) {
     currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
   
@@ -34,11 +36,26 @@ async function apiFetch() {
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
     captionDesc.textContent = desc;
+    temp = weatherData.main.temp;
 
-    let wind = weatherData.wind.speed;
+    wind = weatherData.wind.speed;
     document.querySelector('#wind').textContent = wind;
+    
+    
+    console.log(temp);
+    console.log(wind);
+
+    
+    if (temp <= 50 && wind > 3) {
+      let chill = ((35.74 + (0.6215 * temp))-(35.75 * Math.pow(wind,0.16)) + (0.4275*temp*Math.pow(wind,0.16)));
+      windchill.textContent = chill.toFixed(2);
+      wind.innerHTML = '&#8457;';
+  }else {
+    windchill.innerHTML = "N/A";
 
   };
+
+}
 
 
 
--- lesson10/chamber/scripts/windchill.js
+++ lesson10/chamber/scripts/windchill.js
@@ -1,11 +1,9 @@
 const temp = parseFloat(document.querySelector('#current-temp').textContent);
-// const wind = parseFloat(document.querySelector('#wind').textContent);
+const wind = parseFloat(document.querySelector('#wind').textContent);
 const windchill = document.querySelector('#windchill');
-const wind = document.querySelector('#wind');
-console.log(wind);
-console.log(currentTemp);
+
 if (temp <= 50 && wind > 3) {
-    let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(wind,0.16)) + (0.4275*temp*Math.pow(wind,0.16)));
+    let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(wind,0.16)) + (0.4275**Math.pow(wind,0.16)));
     windchill.textContent = chill.toFixed(0);
     wind.innerHTML = '&#8457;';
 }else {
