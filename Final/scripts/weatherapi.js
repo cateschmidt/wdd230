b20acefd807ee3d60547ed68091cb35c04a6e78e
cateschmidt <c8schmidt@gmail.com>
2023-04-06 00:47:15 -0400

    initial commit

---
+++ Final/scripts/weatherapi.js
@@ -0,0 +1,43 @@
+// Main Weather Card
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const humidity = document.querySelector('#humidity');
+
+
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Carlsbad&units=imperial&appid=2e52b28e193c5dbda364e2b9891de906';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+  
+  function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+    humidity.innerHTML = `<strong>${weatherData.main.humidity.toFixed(0)}</strong>`;
+
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+    // temp = weatherData.main.temp;
+
+}
+
+
+
